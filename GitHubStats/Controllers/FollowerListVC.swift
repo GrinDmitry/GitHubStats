fdb7e35e7c2927b97e2e5595abd915a6ba28c2f2
Dmitry Grin <>
2020-01-14 22:54:39 +0200

    Collection view with diffable data source

--- GitHubStats/Controllers/FollowerListVC.swift
+++ GitHubStats/Controllers/FollowerListVC.swift
@@ -10,28 +10,69 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var followerName: String!
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
+    var followers: [Follower] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        configureDataSource()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func configureViewController() {
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemBackground
-        
-        NetworkManager.shared.getFollowers(for: followerName, page: 1) { result in
+    }
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollowers(for: followerName, page: 1) { [weak self] result in
+            guard let self = self else { return }
             
             switch result {
                 
             case .success(let followers):
-                print(followers)
+                self.followers = followers
+                self.updateData()
                 
             case .failure(let error):
                 self.presentCustomAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "OK")
             }
         }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            navigationController?.setNavigationBarHidden(false, animated: true)
-        }
     }
     
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot,animatingDifferences: true)
+        }
+    }
 }
---
+++ GitHubStats/CustomViews/CustomImageView.swift
@@ -0,0 +1,30 @@
+//
+//  CustomImageView.swift
+//  GitHubStats
+//
+//  Created by Dmitry Grin on 1/14/20.
+//  Copyright © 2020 Dmitry Grin. All rights reserved.
+//
+
+import UIKit
+
+class CustomImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds      = true
+        image              = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
